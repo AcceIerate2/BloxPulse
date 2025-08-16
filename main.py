586553aeb010cfe5ae8f0e8d99114e9edda10b35
Yahya <77327200+YahyaLutta@users.noreply.github.com>
2025-08-16 03:22:13 +0300

    q

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ def loop_scheduler():
             with open(dataFilePath, "r") as f:
                 fileContent = json.load(f)
 
+            print(fileContent)
+
             now = time.time()
             to_delete = []  # collect (uniId, refId) to delete after iter
 
