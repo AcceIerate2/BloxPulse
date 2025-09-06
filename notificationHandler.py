593d7949d9fe85de6ebd4df780976e45513487d7
Yahya <77327200+YahyaLutta@users.noreply.github.com>
2025-09-06 10:06:54 +0300

    more detailed comments

--- notificationHandler.py
+++ notificationHandler.py
@@ -13,6 +13,7 @@ class ReceivedData(TypedDict):
     api_key: str
 
 def pushNotification(data: ReceivedData):
+    print(data)
     """
     Sends a Roblox cloud notification to the user in data['key'].
     """
